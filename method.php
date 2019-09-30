084030eda20f44e84007818b29394545644f1630
HenSwzl <1021298862@qq.com>
2019-09-30 17:53:17 +0800

    Update method.php

--- method.php
+++ method.php
@@ -107,6 +107,7 @@ class Method
     function setUrl() {
         $url_1 = "http://pecl.php.net/package/redis";  //redis官网包
         $url_2 = "https://www.bootcdn.cn/";     //css、js外链地址
+        $url_3 = "https://blog.csdn.net/luanpeng825485697";   //java,c++,c#,python等文章 博客 
     }
 
 
